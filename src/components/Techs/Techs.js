15f63ccef4919fe0759ac2f3a774771f83364870
Dmitrii Rakhmanin <rakhmanin.dm@gmail.com>
2021-06-24 13:41:52 +0300

    feat: add techs adaptivity

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -6,8 +6,7 @@ function Techs({ classes, children }) {
       {children}
       <h3 className="techs__subtitle">7 технологий</h3>
       <p className="techs__text">
-        На курсе веб-разработки мы освоили технологии, которые применили в
-        дипломном проекте.
+        На курсе веб-разработки мы освоили технологии, которые применили в&nbsp;дипломном проекте.
       </p>
       <ul className="techs__techs-list">
         <li className="techs__item">HTML</li>
