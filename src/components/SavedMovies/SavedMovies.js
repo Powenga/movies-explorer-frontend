367c9b9bf8b491b07816d5d8e2dee8f61f1018ad
Dmitrii Rakhmanin <rakhmanin.dm@gmail.com>
2021-07-07 10:34:05 +0300

    feat: add delete error on saved movie page

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,15 +1,30 @@
+import { useContext } from 'react';
+import { ErrorsContext } from '../../contexts/ErrorsContext';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import SeacrchForm from '../SearchForm/SearchForm';
 import MoviesCardWithDelete from '../MoviesCardWithDelete/MoviesCardWithDelete';
 import Preloader from '../Preloader/Preloader';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
 
-function SavedMovies({ classes, userMoviesList, userMovieIsLoading, onCardDelete }) {
+function SavedMovies({
+  classes,
+  userMoviesList,
+  userMovieIsLoading,
+  onCardDelete,
+}) {
+  const {deleteMovieError} = useContext(ErrorsContext);
   return (
     <main className={`main ${classes ? classes : ''}`}>
       <section className="main__section">
         <SeacrchForm classes="main__section-inner" />
       </section>
-      <section className="main__section">
+      <section className="main__section error-wrapper">
+        {deleteMovieError && (
+          <ErrorMessage
+            classes="error-message_active error-message_position_top"
+            text={deleteMovieError}
+          />
+        )}
         {!userMovieIsLoading ? (
           <MoviesCardList
             classes="main__section-inner"
