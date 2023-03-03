67d631b6229294973e1a72fdf12036b1dda0978b
Dmitrii <rakhmanin.dm@gmail.com>
2023-03-03 14:47:10 +0300

    remove old files

--- src/components/ErrorMessage/ErrorMessage.js
+++
@@ -1,9 +0,0 @@
-import './ErrorMessage.css';
-
-function ErrorMessage({classes, text}){
-  return (
-    <p className={`error-message ${classes ? classes : ''}`}>{text}</p>
-  );
-}
-
-export default ErrorMessage;
