7396be5ca4e55fdcf4934f306f81ac3888352b77
Dmitrii <rakhmanin.dm@gmail.com>
2023-03-04 20:49:49 +0300

    ref: Logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,13 +1,22 @@
-import './Logo.css';
 import { Link } from 'react-router-dom';
-import logoPath from '../../images/logo.svg';
+import logoPath from './logo.svg';
+import block from 'bem-css-modules';
+import cn from 'classnames';
+import styles from './Logo.module.css';
+import { FC } from 'react';
 
-function Logo({ classes }) {
+const b = block(styles);
+
+interface Props {
+  classes?: string;
+}
+
+const Logo: FC<Props> = ({ classes }) => {
   return (
-    <Link className={`logo transition ${classes ? classes : ''}`} to="/">
+    <Link className={cn(b(), 'transition', classes)} to="/">
       <img src={logoPath} alt="Логотип" />
     </Link>
   );
-}
+};
 
 export default Logo;
