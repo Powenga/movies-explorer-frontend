18f0f1c7c607eaca4d4036c25b68f947e274b92a
Dmitrii <rakhmanin.dm@gmail.com>
2023-03-05 21:14:05 +0300

    ref: MobileMenu

--- src/contexts/CurrentUserContext.ts
+++ src/contexts/CurrentUserContext.ts
@@ -1,2 +1,8 @@
-import React from "react";
-export const CurrentUserContext = React.createContext();
+import React from 'react';
+type TCurrentUserContext = {
+  currentUser?: { useName: string; userEmail: string; userId: string };
+  loggedIn?: boolean;
+  isUserChecking?: boolean;
+};
+
+export const CurrentUserContext = React.createContext<TCurrentUserContext>({});
