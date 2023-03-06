207cf877bef2aeef6b624a0d89fe107feb425580
Dmitrii <rakhmanin.dm@gmail.com>
2023-03-06 21:31:47 +0300

    ref: SignToggleLink

--- src/components/SignToggleLink/SignToggleLink.tsx
+++ src/components/SignToggleLink/SignToggleLink.tsx
@@ -1,7 +1,14 @@
 import './SignToggleLink';
 import { Link } from 'react-router-dom';
+import { FC } from 'react';
 
-function SignToggleLink({ text, linkText, link }) {
+interface Props {
+  text: string;
+  linkText: string;
+  link: string;
+}
+
+const SignToggleLink: FC<Props> = ({ text, linkText, link }) => {
   return (
     <div className="sign-toggle-link">
       <p className="sign-toggle-link__inner">
@@ -12,6 +19,6 @@ function SignToggleLink({ text, linkText, link }) {
       </p>
     </div>
   );
-}
+};
 
 export default SignToggleLink;
